6ab317405a837f58987d570e9ae0260b92996021
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-30 05:37:37 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-import store from './store'
+import store from './store';
 import vuetify from './plugins/vuetify';
 import '../public/style/main.css'
 import VueSweetalert2 from 'vue-sweetalert2';
